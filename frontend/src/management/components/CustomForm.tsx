9f309c2ddcfa859f269cf4bcf197a207cd8feeec
Andres Gutierrez <gerardoandresg2001@gmail.com>
2025-12-29 22:13:12 -0600

    Add base custom form component structure

--- frontend/src/management/components/CustomForm.tsx
+++ frontend/src/management/components/CustomForm.tsx
@@ -5,15 +5,15 @@ export const CustomForm = () => {
 
     return (
         <div>
-            <table className="w-full border border-gray-400 flex flex-col">
-                <caption className="bg-(--color-principal) text-white text-lg py-2 px-4 text-start rounded-md">
-                    Registro de menores
-                    <button className="ml-10">
+            <table className="w-full flex flex-col">
+                <caption className="bg-(--color-principal) text-white py-2.5 px-4 text-start rounded-t-md flex justify-between">
+                    <p className="text-lg">Registros de menores</p>
+                    <button className="bg-white text-(--color-principal) text-sm font-bold px-4 py-2 rounded-md cursor-pointer">
                         Nuevo Registro
                     </button>
                 </caption>
-                <thead>
-                    <tr className="bg-blue-100 text-(--color-pricipal)">
+                <thead className="">
+                    <tr className="bg-blue-100 text-(--color-pricipal) flex justify-around p-2.5 border-b-2">
                         <th>Persona Menor</th>
                         <th>CURP</th>
                         <th>Fecha Nac.</th>
@@ -25,18 +25,23 @@ export const CustomForm = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="bg-white">
-                        <td>Maria Fernanda</td>
+                    <tr className="bg-white flex justify-around items-center p-2.5 border-b border-gray-400">
+                        <td>
+                            <p className="font-bold">Maria Fernanda</p>
+                            <p className="text-sm">Femenino - 8 anios</p>
+                        </td>
                         <td>324sdfasfewr</td>
                         <td>01/01/2001</td>
                         <td>10/10/2001</td>
                         <td>Casa hogar esperanza</td>
                         <td>DIF municipal</td>
-                        <td>Activo</td>
-                        <td className="flex">
-                            <Pencil />
-                            <Eye />
-                            <Sheet />
+                        <td className="bg-green-200 text-green-800 rounded-2xl px-2 py-0.5">
+                            Activo
+                        </td>
+                        <td className="flex gap-2">
+                            <Pencil className="cursor-pointer"/>
+                            <Eye className="cursor-pointer"/>
+                            <Sheet className="cursor-pointer"/>
                         </td>
                     </tr>
                 </tbody>
